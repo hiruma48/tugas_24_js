31d8bd12434492ac7242af12d4cb83387626ec3c
tri ariyanto <triyanto129@gmail.com>
2023-05-23 05:56:55 +0700

    tugas 24 js
    mengumpulkan tugas js

---
+++ tugas24.js/tugas24.js
@@ -0,0 +1,10 @@
+function panggilangka(){
+    var angka=[32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66 ];
+    console.log("sebelumnya : ",angka);
+    angka1= angka.sort();
+    console.log("Ascending : ",angka1);
+    angka2=angka.reverse();
+    console.log("Descending : ",angka2)
+
+}
+panggilangka();
